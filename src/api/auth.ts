a9c90781142633f016f7205c54de1dad98e3844c
Lovro Klaric <lovro.klaric@binom.hr>
2026-02-26 12:50:59 +0100

    changes in login

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,7 +2,7 @@ import { api } from './axios';
 import type { UserDto, RegisterDto, AuthResponse } from './types';
 
 export const authApi = {
-  login: (credentials: UserDto) => api.create<UserDto, AuthResponse>('/Auth/login', credentials),
+  login: (credentials: UserDto) => api.create<UserDto, AuthResponse>('/api/Auth/login', credentials),
   register: (userData: RegisterDto) => api.create<RegisterDto, any>('/api/Auth/register', userData),
   adminOnly: () => api.getList<any>('/Auth/admin-only'), // Assuming admin-only returns a simple message or status
 };
