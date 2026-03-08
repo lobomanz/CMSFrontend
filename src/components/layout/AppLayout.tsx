96ab3a8a6aa2cc262f6f000b8e37f56820dfbe37
lobomanz <lovro.monkey@gmail.com>
2026-03-08 08:57:33 +0100

    no sidebar

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar';
+// import Sidebar from './Sidebar';
 import Topbar from './Topbar';
 
 const AppLayout: React.FC = () => {
   return (
     <div className="flex h-screen bg-gray-100">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <div className="flex-1 flex flex-col overflow-hidden">
         <Topbar />
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200 p-6">
